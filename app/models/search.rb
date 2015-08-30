abc433432b95433255604081bd6bc07d7fef0ae3
Ruslan Akhmetzyanov <ruslanvall@yandex.ru>
2015-08-30 09:23:36 +0300

    Fix serch model and search tests

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,14 +1,19 @@
 class Search
-  SEARCHING_SOURCES = %w(Question Answer Comment User)
+  SEARCHING_SOURCES = %w(All Question Answer Comment User)
 
-  def self.search(query, source = nil)
+  def self.search(query, source)
     return unless query
+    return [] if source.nil? || !SEARCHING_SOURCES.include?(source)
 
     escaped_query = Riddle::Query.escape(query)
-    classes = []
-    if source && SEARCHING_SOURCES.include?(source)
-      classes << source.classify.constantize
-    end
+
+    classes = source == 'All' ? nil : [source.classify.constantize]
     ThinkingSphinx.search escaped_query, classes: classes
+
+    # classes << []
+    # if source && SEARCHING_SOURCES.include?(source)
+    #   #classes << source.classify.constantize
+    # end
+    # ThinkingSphinx.search escaped_query, classes: classes
   end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -3,18 +3,22 @@ require 'rails_helper'
 RSpec.describe Search, type: :model do
   describe '.search' do
     let(:query) { 'test' }
-    let(:all) { %w(Question Answer User Comment) }
 
     %w(Question Answer User Comment).each do |classes|
       it "should call ThinkingSphinx for classes #{classes}" do
-          expect(ThinkingSphinx).to receive(:search).with(query, {classes: [eval(classes)]})
+          expect(ThinkingSphinx).to receive(:search).with(query, {classes: [classes.constantize]})
           Search.search(query, classes)
       end
     end
 
     it 'should call ThinkingSphinx for classes empty' do
-      expect(ThinkingSphinx).to_not receive(:search).with(query, {classes: ''})
-      Search.search(query, 'Question')
+      expect(ThinkingSphinx).to receive(:search).with(query, {classes: nil})
+      Search.search(query, 'All')
+    end
+
+    it 'should call ThinkingSphinx for classes NoName' do
+      expect(ThinkingSphinx).to_not receive(:search).with(query, {classes: ['NoName']})
+      expect(Search.search(query, 'NoName')).to eq []
     end
   end
 end
