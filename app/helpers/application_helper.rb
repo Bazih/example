4d43c8ea49a5183915669a95e2552686c6c0bdd3
Ruslan Akhmetzyanov <ruslanvall@yandex.ru>
2015-09-04 18:06:36 +0300

    Optimization and caching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def collection_cache_key_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximum(:updated_at).try(:utc)
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+  end
+
+  def cache_key_for_user(model, description)
+    user_id = user_signed_in? ? current_user.id : 0
+    "#{model.to_s.pluralize}/#{description}-#{model.id}-for-user-#{user_id}"
+  end
 end
